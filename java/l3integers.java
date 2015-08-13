1fc4c2a0bb93459b3a39cd923aad03eba9ad77ff
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-12 18:53:29 -0700

    Create l3integers.java

---
+++ java/l3integers.java
@@ -0,0 +1,15 @@
+java> 1;
+java.lang.Integer res0 = 1
+java> "1";
+java.lang.String res1 = "1"
+java> 1 + 1
+java.lang.Integer res2 = 2
+java> 1 - 1;
+java.lang.Integer res3 = 0
+java> 2 * 2;
+java.lang.Integer res4 = 4
+java> 4 / 2;
+java.lang.Integer res5 = 2
+java> 1 + "1"
+java.lang.String res6 = "11"
+java> 3 // 2;
