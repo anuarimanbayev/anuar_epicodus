4088fa696cb41875fd39c8cb1afeba11ad1bf9c7
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-12 18:55:10 -0700

    Create l6javasetup.java

---
+++ java/l6javasetup.java
@@ -0,0 +1,8 @@
+Java setup
+So far, we have been using the javaREPL to do all of our coding exercises in. To write our own Java programs, we'll need to install Java.
+To start, download and install the Java SDK (Standard Edition Development Kit).
+Next, download and install the Java JRE (Runtime Environment).
+To make sure everything is installed correctly, run $ java -version. You should get something like (version numbers do not need to be the same):
+java version "1.8.0_51"
+Java(TM) SE Runtime Environment (build 1.8.0_51-b14)
+Java HotSpot(TM) 64-Bit Server VM (build 25.51-b02, mixed mode)
