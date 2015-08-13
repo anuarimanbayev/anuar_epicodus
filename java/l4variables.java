5c575fea982f37f60b86fd36be73ca84f67f2eec
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-12 18:53:51 -0700

    Create l4variables.java

---
+++ java/l4variables.java
@@ -0,0 +1,32 @@
+java> String phrase = "Hellow world";
+java.lang.String phrase = "Hellow world"
+java> phrase
+java.lang.String phrase = "Hellow world"
+java> phrase + "!";
+java.lang.String res1 = "Hellow world!"
+java> String phrase = "Goodbye world";
+java.lang.String phrase = "Goodbye world"
+java> phrase + "!";
+java.lang.String res2 = "Goodbye world!"
+java> Integer number = 1;
+java.lang.Integer number = 1
+java> number + number;
+java.lang.Integer res4 = 2
+// Java naming convention start with lower case letter, but all subsequent are capitalized, like bigNumber and myFavoriteNumber
+java> Integer bigNumber = 100;
+java.lang.Integer bigNumber = 100
+java> number + bigNumber;
+java.lang.Integer res6 = 101
+java> String myName = "My name is ";
+java.lang.String myName = "My name is "
+java> myName = "Anuar";
+java.lang.String myName = "Anuar"
+// No ; and still worked!
+java> "My name is " + myName
+java.lang.String res8 = "My name is Anuar"
+java> Integer myFavoriteNumber = 56;
+java.lang.Integer myFavoriteNumber = 56
+java> myFavoriteNumber / 2;
+java.lang.Integer res10 = 28
+java> myName + "'s favorite number is " + myFavoriteNumber
+java.lang.String res11 = "Anuar's favorite number is 56"
