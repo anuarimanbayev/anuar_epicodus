1d4d650a7c18d57165be3cbd6ce64d0971ed7226
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-13 12:03:37 -0700

    Update codingisawesome.java

--- java/codingisawesome.java
+++ java/codingisawesome.java
@@ -1,6 +1,7 @@
 /**
  * The codingisawesome class implements an application that
  * simply prints "Coding is awesomesauce!" to standard output.
+ * C:\Users\Anuar\Documents\GitHub\anuar_epicodus\java
  */
 
 public class codingisawesome {
