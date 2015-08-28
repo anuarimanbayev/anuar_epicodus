8b743d968554f8db79d25c7b6e8cf5b07314ad8c
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-28 11:34:03 -0700

    16

---
+++ php/myimanbros.php
@@ -0,0 +1,32 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/myimanbros.php -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/styles.css" rel="stylesheet" type="text/css" media="all">
+    <title>My Brothers Imanbayev</title>
+  </head>
+  <body>
+    <h1>My Iman Bros!!!</h1>
+    <h2>Oh Silly Galym and Daniar!</h2>
+    <img src="img/imanbros2.jpg" alt="A photo of my silly brothers.">
+
+    <!-- Lession 13 Grids incorporating rows and columns -->
+    <!-- Rework one of your existing pages to use Bootstrap's grid -->
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Galym</h3>
+        <p>Here is Galym!!!</p>
+        <img src="img/GALYM.png" alt="A photo of my brother Galym." style="width:150px;height:300px;">
+      </div>
+    
+      <div class="col-md-6">
+        <h3>Daniar</h3>
+        <p>Here is Daniar!!!</p>
+        <img src="img/DANIAR.png" alt="A photo of my brother Daniar." style="width:150px;height:300px;">
+      </div>
+    </div>
+  </body>
+</html>
