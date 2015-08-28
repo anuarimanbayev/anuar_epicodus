b24b7af17b28d478486e54f0f157c55412ca4340
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-28 11:33:55 -0700

    15

---
+++ php/myfirstwebpage.php
@@ -0,0 +1,38 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/myfirstwebpage.php -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+  	<link href="css/styles.css" rel="stylesheet" type="text/css" media="all">
+    <title>Hello world!</title>
+  </head>
+  <body>
+    <h1>My first web page!!!</h1>
+    <h2>Written with the guidance of <em>Epicodus</em></h2>
+
+    <p>
+    Check out
+      <a href="http://www.epicodus.com/">
+      Epicodus
+      </a>
+    . <span>It's a great school for learning web programming!</span>
+    </p>
+
+    <p>Here is a link to my <a href="favoritethings.php">favorite things</a>.</p>
+
+    <img src="img/SnowLeopard.jpg" alt="A photo of a majestic Snow Leopard." style="width:500px;height:300px;">
+
+    <p>This is my <strong>first web page</strong>!</p>
+    <p><span>Isn't it nice?</span></p>
+
+    <p>Here are some things I'm going to learn about coding:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>And a lot more!</li>
+    </ul>
+  </body>
+</html>
