39ab1338c4101e6ff647ba1655e87dc5aab77a0b
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-28 11:34:16 -0700

    17

---
+++ php/phpinfo.php
@@ -0,0 +1,4 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/phpinfo.php -->
+ <?php phpinfo(); ?>
