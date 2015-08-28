eb267d436a03b2b9e9a9725054c96ac898cc6f88
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-28 11:32:45 -0700

    7

--- php/helloworld.php
+++ php/helloworld.php
@@ -1,7 +1,11 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/helloworld.php -->
 <!DOCTYPE html>
 <html>
 <head>
     <title>Hello World!</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <h1>Hello World!</h1>
