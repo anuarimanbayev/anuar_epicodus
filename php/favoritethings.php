ad6db153538dba10c686ce123c467ea99bfc3ae5
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-28 11:32:28 -0700

    5

---
+++ php/favoritethings.php
@@ -0,0 +1,90 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/favoritethings.php -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+  	<link href="css/styles.css" rel="stylesheet" type="text/css" media="all">
+    <title>Favorite Things!</title>
+  </head>
+  <body>
+    <h1>My favorite things!!!</h1>
+    <h2>These are a few of my favorite things</h2>
+
+    <section>
+      <p>Thanks for reading about me!</p>
+    </section>   
+
+    <p>Thanks for reading about me!</p>
+    
+    <div class="column1">
+    <h3>People</h3>
+    <p>Here are some of my favorite <em>people</em>!!!</p>
+    <ul>
+      <li>
+        <a href="myimanbros.php">
+        My brothers Galym and Daniar
+        <img src="img/imanbros.jpg" alt="A photo of my brothers Galym and Daniar" style="width:256px;height:128px;">
+        </a>
+      </li>
+      <li>My mom and dad:
+        <ul>
+          <li>Talapker</li>
+          <li>Jannat</li>
+        </ul>
+       </li>
+
+       <li><span class="brown">My friend Budri</span></li>
+       <li><span class="brown">My friend Raafay</span></li>
+
+       <li><span class="purple">My friend Budri</span></li>
+       <li><span class="purple">My friend Raafay</span></li>
+
+       <li><span class="brown">My friend Budri</span></li>
+       <li><span class="brown">My friend Raafay</span></li>
+      </ul>
+    </div>
+
+    <div class="column2">
+    <h3>Favorite Sports Team</h3>
+    <p>Here is a link to my <a href="favoritesportsteam.php">Favorite Sports Team</a> page.</p>
+    </div>
+
+    <div class="column2">
+    <h3>Flavors of ice cream</h3>
+    <p>These are my three favorites, in order</p>
+    <ol>
+      <li>Vanilla</li>
+      <li>Mocha</li>
+      <li>Chocolate</li>
+    </ol>
+    </div>
+
+    <div class="column3">
+    <h3>Favorite Programming Learning Websites</h3>
+    <p>These are my three favorites, in order</p>
+    <ol>
+      <li>
+        <a href="http://www.epicodus.com/">
+        Epicodus        
+        </a>
+      </li>
+      <li>
+        <a href="http://www.codecademy.com/">
+        <span>Codecademy</span>        
+        </a>
+      </li>
+      <li>
+        <a href="https://www.codeschool.com/">
+        <span>CodeSchool</span>       
+        </a>
+      </li>
+    </ol>
+    </div>
+
+    <footer>
+      <p>Brought to you by the Footer Element</p>
+    </footer>
+  </body>
+</html>
