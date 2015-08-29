61643a271da0f342cf7e3cff9b58b1c5c7ddb520
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-29 12:11:03 -0700

    33

---
+++ php/shipping_calculator.php
@@ -0,0 +1,28 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/shipping_calculator.php -->
+<?php 
+    $input_weight = $_GET["weight"];
+    $input_distance = $_GET["distance"];
+    $shipping_price = calculateShipping($input_weight, $input_distance);
+
+    function calculateShipping($weight, $distance)
+    {
+        $price = $weight / 20 + $distance / 20;      
+        return $price;   
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shipping Calculator</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Price Calculated!</h1>
+        <h3>Your package weighs: <?php echo $input_weight . " lbs"; ?> </h3>
+        <h3>Your package is traveling: <?php echo $input_distance . " miles"; ?> </h3>
+        <h3>The cost will be: $<?php echo $shipping_price; ?> </h3>
+    </div>
+</body>
+</html>
