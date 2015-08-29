37d57bec0922a0a1aa1d0adb60b3b29d77c37e47
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-29 12:07:34 -0700

    8

---
+++ php/encrypt.php
@@ -0,0 +1,26 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/encrypt.php -->
+<?php 
+    $message_to_encode = $_GET["message"];
+    $new_string = encode($message_to_encode);
+
+    function encode($input_phrase)
+    {
+        $reversed_phrase = strrev($input_phrase);
+        $capitalized_phrase = strtoupper($reversed_phrase);
+        return $capitalized_phrase;
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Encrypt a Message</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Message encrypted!</h1>
+        <h2>Here you go: <?php echo $new_string ?></h2>
+    </div>
+</body>
+</html>
