208d5ce48b30190158b63305e95f5ea451c49658
Anuar Imanbayev <anuar.iman@gmail.com>
2015-08-30 10:46:23 -0700

    20

---
+++ php/dictionary5.php
@@ -0,0 +1,56 @@
+<!-- STEP #1: cd C:\Users\Anuar\Documents\GitHub\anuar_epicodus\php -->
+<!-- STEP #2: php -S localhost:8000 -->
+<!-- Step #3: http://localhost:8000/dictionary5.php -->
+
+<!--
+5A. Add 2 more words and definitions to your dictionary.
+5B. Use a foreach loop to print out all the keys and definitions in the browser. The keys should be displayed with <h1> tags and the values should be displayed with <h3> tags.
+-->
+<?php 
+    $word1 = $_GET["word1"];
+    $word2 = $_GET["word2"];
+    $word3 = $_GET["word3"];
+    $word4 = $_GET["word4"];
+    $word5 = $_GET["word5"];
+    $word6 = $_GET["word6"];
+    $word7 = $_GET["word7"];
+    $word8 = $_GET["word8"];
+    $word1_definition = $_GET["word1_definition"];
+    $word2_definition = $_GET["word2_definition"];
+    $word3_definition = $_GET["word3_definition"];
+    $word4_definition = $_GET["word4_definition"];
+    $word5_definition = $_GET["word5_definition"];
+    $word6_definition = $_GET["word6_definition"];
+    $word7_definition = $_GET["word7_definition"];
+    $word8_definition = $_GET["word8_definition"];
+    $dictionary[$word1] = $word1_definition;
+    $dictionary[$word2] = $word2_definition;
+    $dictionary[$word3] = $word3_definition;
+    $dictionary[$word4] = $word4_definition;
+    $dictionary[$word5] = $word5_definition;
+    $dictionary[$word6] = $word6_definition;
+    $dictionary[$word7] = $word7_definition;
+    $dictionary[$word8] = $word8_definition;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <title>Dictionary</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Dictionary</h1>        
+        
+        <?php
+            $dictionary["berkut"] = "golden eagle hunting domesticated bird";
+            $dictionary["atli"] = "steppe pony horse that is short in shoulder height but long in torso.";              
+            foreach ($dictionary as $word => $definition) {
+                $string1 = "<h1>$word: </h1>";
+                $string2 = "<h3>$definition</h3>";
+                echo $string1 . $string2;
+            }
+        ?>
+    </div>
+</body>
+</html>
