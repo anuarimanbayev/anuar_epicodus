78ace39b0a82b9a53a6680f36c310ede8e53c0b5
Anuar Imanbayev <anuar.iman@gmail.com>
2015-09-03 09:35:25 -0700

    32

---
+++ javascript/js/scripts_l18madlibs.js
@@ -0,0 +1,20 @@
+// Lesson 18: JavaScript Debugging
+// Mad Libs ERROR Program Application
+
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+  	
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      //debugger;
+      var userInput = $("input#" + blank).val();
+      console.log(userInput);
+      $("." + blank).text(userInput).val();      
+    });
+
+    $("#story").show();
+    
+    event.preventDefault();
+  });
+});
